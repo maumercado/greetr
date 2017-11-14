2118efa2d5c169ebd34aaf9705c720899c7ff9d9
Mauricio Mercado <mau@maumercado.com>
2017-11-13 17:36:01 -0800

    Updated Example and added comments to it.
    Updated README with a change of description of the
    reason behind creating this small lib.

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ A simple and frienldy javascript lib that reuses jQuery and sets hello as part o
 
 ### Why?
 
-Getting to learn a little bit more of how javascript code can be structured by doing simple things.
+Simple exercise to learn a little bit more of other javascript structures/patterns by doing a simple API.
 
 Want to imitate jQuery structure so that we don't have to call new everytime that we call Greetr.
--- example/app.js
+++ example/app.js
@@ -1,7 +1,18 @@
-var g = G$("Mauricio", "Mercado");
+// let's user our object on the click of the login button
+$("#login").click(function () {
+    // gets a new object (the architecture allows us
+    // to not have to use the 'new' keyword here)
+    // create a new 'Greetr' object
+    var loginGrtr = G$("Mauricio", "Mercado");
 
-g
-    .greet()
-    .setLang("es")
-    .greet(true)
-    .log();
+    // hide the login on the screen
+    $("#logindiv").hide();
+
+    // use our chainable methods
+    // fire off html greeting, passing the '#greeting' selector
+    // and the chosen language, and log the welcome as well
+    loginGrtr
+        .setLang($("#lang").val())
+        .HTMLGreeting("#greeting", true)
+        .log();
+});
